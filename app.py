66f6e82fe30bfb0efe1de99746dee2fc841f22a5
Ellina-Anisimova <73794830+Ellina-Anisimova@users.noreply.github.com>
2024-07-01 14:20:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if image_file is not None:
   d = {"x": x, "y": y}
   # метки строк создаются автоматически
   chart_data = pd.DataFrame(d)
-  st.scatter_chart(chart_data, x="x", y="y", color="#0000FF",size=2)
+  st.scatter_chart(chart_data, x="x", y="y", color="#0000FF",size=6)
   results = process(x_test)
   col1.text(f'Идентификатор пользователя id {results[0]}')
   #col1.image(results[1])
